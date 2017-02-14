1426d4d11216ffaadc8b8a539fb4a3c37e123e8a
Unknown <craig_mcgill@hotmail.com>
2017-02-13 22:12:15 -0500

    Sources UI rickshaw

--- client/templates/sources/sources.js
+++ client/templates/sources/sources.js
@@ -1,6 +1,13 @@
 Meteor.subscribe('sources');
 Meteor.subscribe('postsBySource');
 
+Template.sources.rendered = function () {
+  
+  Session.set('selectedSource', '');
+  Session.set('selectedName', '');
+  Session.set('graphClicked', false)
+}
+
 Template.sources.helpers({
   'listSources': function(){
       var currentUserId = Meteor.userId();
@@ -9,8 +16,29 @@ Template.sources.helpers({
   'postsBySource': function() {
     return Posts.find({author: this.author});
   },
+  'selectedClass': function(){
+    var sourceId = this._id;
+    var selectedSource = Session.get('selectedSource');
+    if (sourceId === selectedSource) {
+      return "selected"
+    }
+    },
+  'showSelectedPosts': function () {
+    var currentUserId = Meteor.userId();
+    var selectedSource = Session.get('selectedSource');
+    var selectedName = Session.get('selectedName');
+    return Posts.find({author: selectedName});
+  },
   'postCount': function() {
     return Posts.find({author: this.author}).count();
+  },
+  'showGraph': function() {
+    if (Session.get('graphClicked') === true) {
+      return 'visible'
+    }
+    else return 'hidden'
+
+
   },
   'sourceID': function () {
     return this.author
@@ -28,3 +56,17 @@ Template.sources.helpers({
     }
   }
 });
+
+Template.sources.events({
+  'click .source': function () {
+    var sourceId = this._id;
+    var sourceName = this.author;
+    console.log(sourceId);
+    Session.set('selectedSource', sourceId);
+    Session.set('selectedName', sourceName)
+  },
+  'click .graph-button': function () {
+    //var graphClicked = True;
+    Session.set('graphClicked', true);
+  }
+})
